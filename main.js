ec9bfc6c2a74be82f66f95749a867c5983e698c5
quentonw <quenton.whitecalf@edu.sait.ca>
2025-08-12 20:54:37 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,4 @@
-@media screen and (max-width: 570px) {
-    button {
-        display: block;
-    }
-  
-    nav {
-       /* display: none; */
-       position: absolute;
-       top: 100%;
-       right: 0;
-       z-index: 100;
-       background-color: #333;
-       /* width: 100%; */
-    }
-  
-    nav ul {
-        flex-direction: column;
-        gap: 10px;
-    }
-  
-    nav.active {
-        display: block;
-    }
-}
+
 
 var navContainer = document.getElelemntById("menu");
 var btn = document.getElementById("toggle");
