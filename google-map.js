9e4c84f6e25113075d2acd98a149aef2ea133313
quentonw <quenton.whitecalf@edu.sait.ca>
2025-08-13 20:46:52 -0600

    Update google-map.js

--- google-map.js
+++ google-map.js
@@ -17,3 +17,55 @@ function loadScript() {
     document.body.appendChild(script);
 
 window.onload = loadScript;
+
+import React, {useEffect} from 'react';
+import {APIProvider, useMap} from '@vis.gl/react-google-maps';
+
+const MyComponent = () => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (!map) return;
+
+    // here you can interact with the imperative maps API
+  }, [map]);
+
+  return <></>;
+};
+
+const App = () => (
+  <APIProvider apiKey={'YOUR API KEY HERE'}>
+    <Map /* ... */></Map>
+
+    <MyComponent />
+  </APIProvider>
+);
+
+import {useMapsLibrary} from '@vis.gl/react-google-maps';
+
+const MyComponent = () => {
+  const map = useMap();
+
+  // triggers loading the places library and returns the API Object once complete (the
+  // component calling the hook gets automatically re-rendered when this is
+  // the case)
+  const placesLibrary = useMapsLibrary('places');
+
+  const [placesService, setPlacesService] = useState(null);
+
+  useEffect(() => {
+    if (!placesLibrary || !map) return;
+
+    // when placesLibrary is loaded, the library can be accessed via the
+    // placesLibrary API object
+    setPlacesService(new placesLibrary.PlacesService(map));
+  }, [placesLibrary, map]);
+
+  useEffect(() => {
+    if (!placesService) return;
+
+    // ...use placesService...
+  }, [placesService]);
+
+  return <></>;
+};
