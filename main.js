33648d41db1e8404a1ce1d24f25860e14299411f
quentonw <quenton.whitecalf@edu.sait.ca>
2025-08-13 20:27:19 -0600

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,3 @@ btn.addEventListener('click', toggleMenu);
 
 const exploreBtn = document.getElementById('exploreMoreBtn');
 exploreBtn.addEventListener('click', scrollToMain);
-
-GOOGLE_MAPS_API_KEY=<your API key here>
-
